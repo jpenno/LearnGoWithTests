da4a38ca8019a75cb0d2dd075e2c7affd679c894
jpenn <jpenno123@hotmail.com>
2024-05-28 10:18:34 +1000

    refactor pointers
    add bitcoin to wallet

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -11,11 +10,10 @@ func TestWallet(t *testing.T) {
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
