5066b4c489be7f510e5ef3c0ca83fe8b3b1aebbc
jpenn <jpenno123@hotmail.com>
2024-05-28 22:11:44 +1000

    add test for unknown word

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,17 +4,29 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+		assertStrings(t, got, want)
+	})
 
-	assertStrings(t, got, want)
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknowen")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
