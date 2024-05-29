de853d29247cd96cb9829c030b01f55d6f7d4e6d
jpenn <jpenno123@hotmail.com>
2024-05-29 22:35:54 +1000

    add configurable sleeper

--- mocking/main.go
+++ mocking/main.go
@@ -13,12 +13,21 @@ const (
 )
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
 type Sleeper interface {
 	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 type DefaultSleeper struct{}
