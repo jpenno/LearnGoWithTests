6969dd0dc17eaa376747a1f6bf3dfe63b321e0b0
jpenn <jpenno123@hotmail.com>
2024-05-29 13:38:00 +1000

    refactor

--- di/di.go
+++ di/di.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func main() {
+	Greet(os.Stdout, "jason")
+}
+
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
