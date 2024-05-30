3404e2382e49d73464bf340c9f2dc5f19cdabdef
jpenn <jpenno123@hotmail.com>
2024-05-30 15:13:26 +1000

    refactor

--- select/racer.go
+++ select/racer.go
@@ -1,16 +1,25 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
+var tenSecondTimeOut = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeOut)
+}
+
+func ConfigurableRacer(a, b string, timeOut time.Duration) (winner string, error error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeOut):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
