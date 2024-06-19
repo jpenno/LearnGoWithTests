15b963b1709b6569c70dd0049362b04a943b45fd
jpenn <jpenno123@hotmail.com>
2024-06-19 19:29:05 +1000

    finish roma-numerals

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
-func TestRomnNumerals(t *testing.T) {
-	cases := []struct {
-		Arabic int
+var (
+	cases = []struct {
+		Arabic uint16
 		Roman  string
 	}{
 		{Arabic: 1, Roman: "I"},
@@ -40,6 +41,9 @@ func TestRomnNumerals(t *testing.T) {
 		{Arabic: 1006, Roman: "MVI"},
 		{Arabic: 798, Roman: "DCCXCVIII"},
 	}
+)
+
+func TestRomnNumerals(t *testing.T) {
 	for _, test := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
@@ -50,28 +54,31 @@ func TestRomnNumerals(t *testing.T) {
 	}
 }
 
-// func ConvertToRoman(arabic int) string {
-// 	var result strings.Builder
-//
-// 	for arabic > 0 {
-// 		switch {
-// 		case arabic > 9:
-// 			result.WriteString("X")
-// 			arabic -= 10
-// 		case arabic > 8:
-// 			result.WriteString("IX")
-// 			arabic -= 9
-// 		case arabic > 4:
-// 			result.WriteString("V")
-// 			arabic -= 5
-// 		case arabic > 3:
-// 			result.WriteString("IV")
-// 			arabic -= 4
-// 		default:
-// 			result.WriteString("I")
-// 			arabic--
-// 		}
-// 	}
-//
-// 	return result.String()
-// }
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %q", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got: %d, want: %d", got, test.Arabic)
+			}
+		})
+	}
+}
+
+func TestPropetiesOfConversion(t *testing.T) {
+	attertion := func(arabic uint16) bool {
+		if arabic > 3_999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(attertion, &quick.Config{
+		MaxCount: 1_000,
+	}); err != nil {
+		t.Error("failed checks", err)
+	}
+}
